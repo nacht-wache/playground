b612ea4311405ea59e24adc1b0d49f0b63ec3ab6
Ivan Mikhalchuk <nachtwache1889@outlook.com>
2025-10-08 00:39:02 +0300

    improve exception safety and guaranties in MemoryPool.hpp

--- MemoryPool/MemoryPool.hpp
+++ MemoryPool/MemoryPool.hpp
@@ -8,12 +8,12 @@
 template <typename T, typename FreeList = FreeList<T>>
 // :(
 // todo fix
-requires (alignof(T) >= alignof(uintptr_t))
+  requires(alignof(T) >= alignof(uintptr_t))
 class MemoryPool {
   inline constinit static auto kAlignment = std::alignment_of_v<T>;
   inline constinit static auto kSize = sizeof(T);
 
-public:
+ public:
   struct FreeBlockDeleter {
     MemoryPool& pool;
 
@@ -22,19 +22,19 @@ public:
     }
   };
 
-public:
+ public:
   using Unique = std::unique_ptr<T, FreeBlockDeleter>;
 
-public:
+ public:
   explicit MemoryPool(std::size_t size) noexcept
-    : m_data(static_cast<std::byte*>(operator new[](
-          size * kSize, std::align_val_t{kAlignment})))
-    , m_freeList(m_data, size){
+      : m_data(static_cast<std::byte*>(operator new[](
+            size* kSize, std::align_val_t{kAlignment}))),
+        m_freeList(m_data, size) {
   }
 
   ~MemoryPool() noexcept {
     // it's up to user to return all ptrs for destruction
-    operator delete [](m_data, std::align_val_t{kAlignment});
+    operator delete[](m_data, std::align_val_t{kAlignment});
   }
 
   // Non-copyable
@@ -48,20 +48,48 @@ public:
 
   MemoryPool& operator=(MemoryPool&& other) noexcept = delete;
 
-public:
+ public:
+  // since C++17 we can overload based on noexcept specifier
+  // strong exception-safety guarantee
   template <typename... U>
-  [[nodiscard]] T* Allocate(U&&... args) {
+  [[nodiscard]] T* Allocate(U&&... args) noexcept(
+      std::is_nothrow_constructible_v<T, U...>) {
     auto freeBlock = m_freeList.Pop();
     if (freeBlock == nullptr) {
       return nullptr;
     }
 
-    new(freeBlock) T(std::forward<U>(args)...);
+    // strong exception-safety guarantee
+    try {
+      new (freeBlock) T(std::forward<U>(args)...);
+    } catch (std::exception& e) {
+      m_freeList.Push(freeBlock);
+      std::cout << e.what() << std::endl;
+      return nullptr;
+    } catch (...) {
+      m_freeList.Push(freeBlock);
+      return nullptr;
+    }
+
+    return freeBlock;
+  }
+
+  // since C++17 we can overload based on noexcept specifier
+  template <typename... U>
+    requires std::is_nothrow_constructible_v<T, U...>
+  [[nodiscard]] T* Allocate(U&&... args) noexcept {
+    auto freeBlock = m_freeList.Pop();
+    if (freeBlock == nullptr) {
+      return nullptr;
+    }
+
+    new (freeBlock) T(std::forward<U>(args)...);
     return freeBlock;
   }
 
   template <typename... U>
-  [[nodiscard]] Unique AllocateSmart(U&&... args) {
+  [[nodiscard]] Unique AllocateSmart(U&&... args) noexcept(
+      noexcept(Allocate(std::forward<U>(args)...))) {
     return CreateUnique(Allocate(std::forward<U>(args)...));
   }
 
@@ -70,19 +98,19 @@ public:
     m_freeList.Push(ptr);
   }
 
-private:
+ private:
   [[nodiscard]] FreeBlockDeleter CreateDeleter() noexcept {
     return {.pool = *this};
   }
 
-  [[nodiscard]] Unique CreateUnique(T* ptr) {
+  [[nodiscard]] Unique CreateUnique(T* ptr) noexcept {
     return Unique{ptr, CreateDeleter()};
   }
 
-private:
+ private:
   std::byte* m_data;
   FreeList m_freeList;
 };
 
-template <typename T, template<typename> typename FreeList>
+template <typename T, template <typename> typename FreeList>
 using UniqueV = std::vector<typename MemoryPool<T, FreeList<T>>::Unique>;
